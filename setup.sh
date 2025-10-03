0de7010254d5985007290c834e3f3549fad0576a
lvaughan <luke.vaughan@okstate.edu>
2025-10-03 16:41:23 -0500

    clean up tmp dir

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 export PDF_NAME="thesis"
 export SERVER_PORT=8080
 
+mkdir -p tmp
+
 # Start the HTTP server for viewing the pdf
 if lsof -i :$SERVER_PORT > /dev/null; then
     echo "Starting server failed! Port $SERVER_PORT is already in use."
